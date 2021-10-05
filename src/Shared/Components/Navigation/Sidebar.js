394237c4895ab865d08b50941f3153f0869ea2ae
Pedro Scarton <pedroscartonr@gmail.com>
2021-10-05 00:08:15 -0300

    sidebard done

--- src/Shared/Components/Navigation/Sidebar.js
+++ src/Shared/Components/Navigation/Sidebar.js
@@ -1,15 +1,48 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import classes from './Sidebar.module.css'
+import classes from './Sidebar.module.css';
 
-
-
+const SideBarItem = (props) => {
+	return (
+		<li className={classes.item}>
+			<NavLink
+				className={classes.itemLink}
+				activeClassName={classes.itemLinkActive}
+				exact
+				to={props.href}
+			>
+				<props.icon />
+				<p>{props.name}</p>
+				{props.notification && (
+					<div className={classes.countItem}>
+						<p>{props.counte}</p>
+					</div>
+				)}
+			</NavLink>
+		</li>
+	);
+};
 const Sidebar = (props) => {
-    return ( 
-        <aside className={classes.sideBar}>
-            
-        </aside>
-     );
-}
- 
+	console.log(props.tools);
+	return (
+		<aside className={classes.sideBar}>
+			<div>
+				<nav>
+					<ul>
+						{props.tools &&
+							props.tools.map((tool) => (
+								<SideBarItem
+									key={tool.name}
+									{...tool}
+									count={props.count}
+								/>
+							))}
+					</ul>
+				</nav>
+			</div>
+		</aside>
+	);
+};
+
 export default Sidebar;
