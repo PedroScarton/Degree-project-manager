43b80088c516da68a2a07b90e1256612f45e91ee
LePeripatetic <43622638+LePeripatetic@users.noreply.github.com>
2021-10-06 22:09:00 -0300

    Card

--- src/App.js
+++ src/App.js
@@ -6,13 +6,38 @@ import { AuthContext } from './Shared/Context/auth-context';
 import { useAuth } from './Shared/Hooks/auth-hook';
 
 import MainWrapper from './Shared/Components/Layout/MainWrapper';
+import Input from './Shared/Components/FormElements/input';
+
 
 const App = () => {
 	const { token, id, name, lastname, email, rut, imageUrl, role, login, logout } =
-	useAuth();
-	
+		useAuth();
+
+
 	// aca adentro pon tus componentes para la prueba
-	return ();
+	return (
+		<div>
+
+			<Input
+				type="password"
+				label="Rut"
+				helperText="Aquí va el rut"
+				placeholder="20.206.812-7"
+			/>
+
+			<Input
+				type="date"
+				label="Fecha de evaluación: *"
+				helperText="La fecha de evaluación debe ser posterior a la fecha de inicio."
+			/>
+
+			<Input
+				type="file"
+				label="Documento de evaluación: *"
+				helperText="Por favor subir el archivo Word completado descargado previamente."
+			/>
+		</div>
+	);
 
 	// return (
 	// 	<AuthContext.Provider
--- src/Shared/Components/UI/Card.js
+++ src/Shared/Components/UI/Card.js
@@ -1,5 +1,13 @@
+import React from "react";
+
+import './Card.css'
+
 const Card = (props) => {
-	return <div className>{props.children}</div>;
+	return (
+		<div className="card-container" style={{border: `1.5px solid ${props.color}`}}>
+			{props.children}
+		</div>
+	);
 };
 
 export default Card;
